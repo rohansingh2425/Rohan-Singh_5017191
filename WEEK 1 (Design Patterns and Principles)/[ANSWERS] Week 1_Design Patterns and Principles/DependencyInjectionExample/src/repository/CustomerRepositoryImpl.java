1c3c0270bed6be19430705b5885a41fc0e1337ee
Rohan Singh <113093221+rohansingh2425@users.noreply.github.com>
2024-07-31 15:54:28 +0530

    Update CustomerRepositoryImpl.java

--- WEEK 1 (Design Patterns and Principles)/[ANSWERS] Week 1_Design Patterns and Principles/DependencyInjectionExample/src/repository/CustomerRepositoryImpl.java
+++ WEEK 1 (Design Patterns and Principles)/[ANSWERS] Week 1_Design Patterns and Principles/DependencyInjectionExample/src/repository/CustomerRepositoryImpl.java
@@ -3,8 +3,7 @@ package repository;
 public class CustomerRepositoryImpl implements CustomerRepository {
     @Override
     public Customer findCustomerById(int id) {
-        // In a real application, this would interact with a database.
-        // For simplicity, we're returning a dummy customer.
+        
         return new Customer(id, "John Doe");
     }
 }
