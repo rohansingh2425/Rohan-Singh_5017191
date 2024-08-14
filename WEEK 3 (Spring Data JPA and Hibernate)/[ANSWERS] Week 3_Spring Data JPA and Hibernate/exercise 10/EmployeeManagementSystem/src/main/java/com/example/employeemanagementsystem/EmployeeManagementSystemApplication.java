637cc2617c17820d8d46c2dda282fc7353430b2f
Rohan Singh <113093221+rohansingh2425@users.noreply.github.com>
2024-08-14 12:16:42 +0530

    Add files via upload

---
+++ WEEK 3 (Spring Data JPA and Hibernate)/[ANSWERS] Week 3_Spring Data JPA and Hibernate/exercise 10/EmployeeManagementSystem/src/main/java/com/example/employeemanagementsystem/EmployeeManagementSystemApplication.java
@@ -0,0 +1,40 @@
+package com.example.employeemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import java.util.List;
+
+
+@SpringBootApplication
+public class EmployeeManagementSystemApplication implements CommandLineRunner {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EmployeeManagementSystemApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception
+    {
+        Department dept = new Department();
+        dept.setName("IT");
+        departmentRepository.save(dept);
+
+        Employee emp = new Employee();
+        emp.setName("Rohan Singh");
+        emp.setEmail("rohansingh@gmail.com");
+        emp.setDepartment(dept);
+        employeeRepository.save(emp);
+
+        List<Employee> employees = employeeRepository.findByDepartmentName("IT");
+        employees.forEach(System.out::println);
+    }
+}
