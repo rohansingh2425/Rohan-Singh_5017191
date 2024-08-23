08806ef3d02686e635d9b5ae1c7119d3259ecf95
Rohan Singh <113093221+rohansingh2425@users.noreply.github.com>
2024-08-23 10:58:35 +0530

    Add files via upload

---
+++ WEEK 4 (Spring REST using Spring Boot 3)/[ANSWERS] Week 4_Spring REST/exercise 07/BookstoreAPI/src/main/java/com/bookstore/entity/Customer.java
@@ -0,0 +1,16 @@
+package com.bookstore.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer
+{
+    private Long id;
+    private String name;
+    private String email;
+    private String address;
+}
